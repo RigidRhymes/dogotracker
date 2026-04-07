0862bcfee7c79db58ee843ad0c84f815fb274217
RhymesR <allmicandfame@gmail.com>
2026-04-07 13:56:11 +0100

    reset project

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,30 +1,8 @@
-import Header from "@/components/Header"
-import CardLong from "@/components/Lon-Card"
-import {headers} from "next/headers";
-import {redirect} from "next/navigation";
-import {getAuth} from "@/lib/better-auth/auth";
+import React from 'react'
 
-
-
-const Layout = async ({children} : {children: React.ReactNode}) => {
-    const auth = await getAuth();
-    const session = await auth.api.getSession({headers: await headers()})
-    if(!session?.user) redirect('/sign-up')
-
-    const user = {
-        id: session.user.id,
-        name: session.user.name,
-        email: session.user.email,
-    }
-  return (
-    <main className="min-h-screen">
-        <Header user={user}/>
-        <div>
-            {children}
-        </div>
-        <CardLong/>
-    </main>
-  )
+const Layout = () => {
+    return (
+        <div>Layout</div>
+    )
 }
-
 export default Layout
